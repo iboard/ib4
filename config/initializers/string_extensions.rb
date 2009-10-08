b4745d2b38903f6f17797b94c56454b7244cb15f
Andi Altendorfer <andi@iboard.cc>
2009-10-08 23:13:21 +0200

    Fix to_txt - see http://ib4.iboard.cc/postings/17

--- config/initializers/string_extensions.rb
+++ config/initializers/string_extensions.rb
@@ -1,7 +1,8 @@
 class String
   
   define_method(:to_txt) { 
-    textilize(self.to_s.gsub(/[&><]/) { |special| { '>' => '&gt;', '<' => '&lt;', '"' => '&quot;' }[special] })
+    textilize(self.to_s.gsub(/(Script|\?php)/i, '*NO SCRIPTING (\1)*'))
+    #gsub(/[&><]/) { |special| { ‘>’ => ‘>’, ‘<’ => ‘<’, ‘"’ => ‘"’ }[special] }) } 
   }
 
 end
