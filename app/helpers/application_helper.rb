f16b6ee60cb610e0a88ddd90d9e52ec03f6b4f9f
Andi Altendorfer <andi@iboard.cc>
2009-10-10 17:26:51 +0200

    Add is_destination? to permalink to enable highlighting of actual displayed url in links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+   # return true if url is part of the actual requested url
+   
+   def is_active_url?(url)
+     if @page && @page.permalinks.any?
+       @page.permalinks.detect{|u| u.permalinkable.url == url }
+     else
+       request.request_uri.eql?(url)
+     end
+   end
+   
+   
 end
 
    
