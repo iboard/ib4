57815e7c0fec4ff3e65c6d364cc9fef7d8af2fca
Andi Altendorfer <andi@iboard.cc>
2009-10-11 20:03:29 +0200

    Bugfix

--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -1,5 +1,7 @@
 module UserSessionsHelper
   def current_locale
-    session['locale'] || DEFAULT_LOCALE
+    return session['locale'] unless session['locale'].blank?
+    browservalue = request.env['HTTP_ACCEPT_LANGUAGE'] ? request.env['HTTP_ACCEPT_LANGUAGE'].gsub(/\-.*$/,"") : DEFAULT_LOCALE
+    session['locale'] || browservalue
   end
 end
