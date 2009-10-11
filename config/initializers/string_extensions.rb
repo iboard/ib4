7fd7f217a43ef73cc4c087b8f43f78dc3bcf1f66
Andi Altendorfer <andi@iboard.cc>
2009-10-11 17:48:57 +0200

    Bugfix to_txt

--- config/initializers/string_extensions.rb
+++ config/initializers/string_extensions.rb
@@ -1,7 +1,7 @@
 class String
   
   define_method(:to_txt) { 
-    textilize(self.to_s.gsub(/(Script|\?php)/i, '*NO SCRIPTING (\1)*'))
+    textilize(self.to_s.gsub(/<(Script|\?php)/i, '*NO SCRIPTING (\1)*'))
     #gsub(/[&><]/) { |special| { ‘>’ => ‘>’, ‘<’ => ‘<’, ‘"’ => ‘"’ }[special] }) } 
   }
 
