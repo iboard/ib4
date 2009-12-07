5997d1afdc1227311b2d99dfcb6d798d28a475f2
Andi Altendorfer <andi@iboard.cc>
2009-12-07 15:39:18 +0100

    Bugfix with avatar in message notification

--- lib/message_postman.rb
+++ lib/message_postman.rb
@@ -7,7 +7,7 @@ class MessagePostman < Struct.new(:message_notification_id,:subject_txt)
        notify.user.email,
        subject_txt,
        notify.message.message,
-       "http://#{ROOT_URL}/#{notify.message.user.avatar.url(:avatar)}"
+       "#{ROOT_URL}#{notify.message.user.avatar.url(:avatar)}"
     ) 
   end
 end
