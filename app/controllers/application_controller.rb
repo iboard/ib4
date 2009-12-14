182a884fa533b4a4cf70e9af7ecfc91c1ef56e2b
Andi Altendorfer <andi@iboard.cc>
2009-12-14 15:01:29 +0100

    Layout simplified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,7 +55,6 @@ class ApplicationController < ActionController::Base
     end
   end
   
-  
   def is_admin?
     current_user && current_user.is_admin?
   end
--- lib/randomizer.rb
+++ lib/randomizer.rb
@@ -1,15 +1,20 @@
 module Randomizer
   
-  RANDOM_CHARACTERS="abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ0123456789"
+  RANDOM_CHARACTERS="abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ0123456789-_"
+  RANDOM_CHARACTERS_EXTENDED="abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_$%,;!#*+"
   
   def self.version
-    "RANDOMIZER MODULE 1.1"
+    "RANDOMIZER MODULE 1.1.a"
   end
   
-  def self.randstr(length=8)
+  def self.randstr(length=8,urlsafe=true)
     rc = ""
     length.times do 
-      rc << RANDOM_CHARACTERS[rand(RANDOM_CHARACTERS.length).round]
+      if urlsafe
+        rc << RANDOM_CHARACTERS[rand(RANDOM_CHARACTERS.length).round]
+      else
+        rc << RANDOM_CHARACTERS_EXTENDED[rand(RANDOM_CHARACTERS.length).round]        
+      end
     end
     rc
   end
