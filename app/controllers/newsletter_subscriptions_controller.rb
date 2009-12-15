e2c7440af396aa10d06d43f8ad271ee18a09cab7
Andi Altendorfer <andi@iboard.cc>
2009-12-15 22:13:14 +0100

    Newsletter Layoutfix

--- app/controllers/newsletter_subscriptions_controller.rb
+++ app/controllers/newsletter_subscriptions_controller.rb
@@ -2,7 +2,6 @@ class NewsletterSubscriptionsController < ApplicationController
   
   before_filter :load_newsletter
   before_filter :require_admin, :except => [:create,:destroy]
-  before_filter :require_user
 
   def index
     @newsletter_subscriptions = @newsletter.newsletter_subscriptions
