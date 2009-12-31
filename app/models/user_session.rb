b53d4cb8da36faecea8aac6bd7df4d5578a41d3c
Andi Altendorfer <andi@iboard.cc>
2009-12-31 14:17:26 +0100

    Bugfix Session Notes for new users

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -6,32 +6,40 @@ class UserSession < Authlogic::Session::Base
   
   private
   def create_login_note
-    if last_login && last_login.children.empty?
-      logout_note = last_login.children.create(:message_type => :end_action, 
-                                 :message => 'LOG OUT (autom. session reset at login)', 
-                                 :message_value => Time.now.to_i.to_s, 
-                                 :parent => last_login, 
-                                 :user_id => user.id,
-                                 :noteable_type => 'User', :noteable_id => user.id)
-      logout_note.save!
+    begin
+      if last_login && last_login.children.empty?
+        logout_note = last_login.children.create(:message_type => :end_action, 
+                                   :message => 'LOG OUT (autom. session reset at login)', 
+                                   :message_value => Time.now.to_i.to_s, 
+                                   :parent => last_login, 
+                                   :user_id => user.id,
+                                   :noteable_type => 'User', :noteable_id => user.id)
+        logout_note.save!
+      end
+  
+      @last_login = user.notes.create(:message_type => :new_action, :message => 'LOG IN', :message_value => Time.now.to_i.to_s, 
+                      :user_id => user.id, :noteable_type => 'User', :noteable_id => user.id)
+      @last_login.save!
+    rescue
+      logger.info("*** FIRST SESSION CREATE AFTER REGISTRATION ***")
     end
-
-    @last_login = user.notes.create(:message_type => :new_action, :message => 'LOG IN', :message_value => Time.now.to_i.to_s, 
-                    :user_id => user.id, :noteable_type => 'User', :noteable_id => user.id)
-    @last_login.save!
   end
   
   def create_logout_note
-    if !last_login
-      @last_login = user.notes.create(:message_type => :new_action, :message => 'LOG IN (autom. session login at logout)', 
-                        :message_value => Time.now.to_i.to_s, 
-                        :user_id => user.id, :noteable_type => 'User', :noteable_id => user.id)
-      @last_login.save!
-      
+    begin
+      if !last_login
+        @last_login = user.notes.create(:message_type => :new_action, :message => 'LOG IN (autom. session login at logout)', 
+                          :message_value => Time.now.to_i.to_s, 
+                          :user_id => user.id, :noteable_type => 'User', :noteable_id => user.id)
+        @last_login.save!
+        
+      end
+      n = user.notes.create(:message_type => :end_action, :message => 'LOG OUT', :message_value => Time.now.to_i.to_s, 
+                        :parent => last_login, :noteable_type => 'User', :noteable_id => user.id)
+      n.save!
+    rescue
+      logger.info("*** FIRST SESSION DESTROY AFTER REGISTRATION ***")
     end
-    n = user.notes.create(:message_type => :end_action, :message => 'LOG OUT', :message_value => Time.now.to_i.to_s, 
-                      :parent => last_login, :noteable_type => 'User', :noteable_id => user.id)
-    n.save!
   end
   
   def last_login
--- app/models/usergroup.rb
+++ app/models/usergroup.rb
@@ -8,7 +8,7 @@ class Usergroup < ActiveRecord::Base
   named_scope :with_join_mask, lambda { |mask| {:conditions => "join_mask & #{mask} > 0 "} }  
 
   validates_presence_of :name
-  validates_uniqueness_of :name  
+  validates_uniqueness_of :name, :scope => :user_id
   
   after_create  :create_memberships
   after_save    :create_memberships
